82e82d3f6eb311b1b4fa4ed7d5a7f3e84cba061f
Mark Casaje <mcasaje@users.noreply.github.com>
2018-07-22 21:46:21 -0700

    Added sample project
    Adding sample project with Spring Boot, Hibernate, H2, and PostgreSQL

---
+++ README.md
@@ -0,0 +1,7 @@
+# RESTful Microservice Quickstart
+
+To package the service, run `mvn clean package` to create an executable jar file.
+To launch the service locally, run `java -jar target/<name-of-jar>.jar`.
+
+For more details, please read
+[Building a RESTful Web Service](http://spring.io/guides/gs/rest-service/).
---
+++ src/main/java/com/sample/greeting/Greeting.java
@@ -0,0 +1,28 @@
+package com.sample.greeting;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Greeting {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
